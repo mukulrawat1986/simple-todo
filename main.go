30b03b9afa38b378e4a6f0fd4134bdb837ae8241
Mukul Rawat <mukulsrawat@gmail.com>
2016-07-17 18:52:44 +0530

    a simple todo app

--- main.go
+++ main.go
@@ -1,22 +1,50 @@
+// our simple ToDo app
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"html"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
+
+// our Model
+type Todo struct {
+	Name      string
+	Completed bool
+	Due       time.Time
+}
+
+type Todos []Todo
 
 func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", Index)
-	log.Fatal(http.ListenAndServe(":8080", router))
+	router.HandleFunc("/todos", TodoIndex)
+	router.HandleFunc("/todos/{todoId}", TodoShow)
 
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	fmt.Fprintln(w, "Welcome!")
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	todos := Todos{
+		Todo{Name: "Write Presentation"},
+		Todo{Name: "Host meetup"},
+	}
+
+	json.NewEncoder(w).Encode(todos)
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoId := vars["todoId"]
+	fmt.Fprintln(w, "Todo show:", todoId)
 }
