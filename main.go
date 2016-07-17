b42eb2ac1f2cca73763f8444c294dd1a74357fc8
Mukul Rawat <mukulsrawat@gmail.com>
2016-07-17 13:27:41 +0530

    a simple webserver that outputs the request url

---
+++ main.go
@@ -0,0 +1,19 @@
+// A simple webserver that responds to any requests by simply outputting
+// the request url
+
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
