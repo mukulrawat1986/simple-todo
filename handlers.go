43177aa6349108f36c17b605e9ee65b1925869b3
Mukul Rawat <mukulsrawat@gmail.com>
2016-07-17 21:27:07 +0530

    return content-type and set headers

--- handlers.go
+++ handlers.go
@@ -16,6 +16,9 @@ func TodoIndex(w http.ResponseWriter, r *http.Request) {
 		Todo{Name: "Host Meetup"},
 	}
 
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOk)
+
 	if err := json.NewEncoder(w).Encode(todos); err != nil {
 		log.Fatalf("Error while encoding todo into json")
 		os.Exit(1)
